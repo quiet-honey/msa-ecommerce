038b96038426e7060b6056c703cd19f9ff2602f0
quiet-honey <skap0329@naver.com>
2024-10-26 11:49:10 +0900

    init project

---
+++ services/order-service/src/test/java/com/quiet_honey/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiet_honey.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

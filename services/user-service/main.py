0e577881ff836c5f08005394d9d0b80d9924be04
quiet-honey <skap0329@naver.com>
2024-11-03 21:53:18 +0900

    fix typo

--- services/user-service/main.py
+++ services/user-service/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-from core.db import get_user_col
+from core.db import get_users_col
 
 app = FastAPI()
 
 
 @app.get("/users")
 async def test():
-    await get_user_col().insert_one({"test": "test"})
+    await get_users_col().insert_one({"test": "test"})
     return "user-service-test"
